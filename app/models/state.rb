98c1f850a63569606392cdcea523e9d8841c84a1
edatrix <lespath@gmail.com>
2015-02-22 21:00:12 -0700

    adds states controller, resource, starts moving parser to state model

--- app/models/state.rb
+++ app/models/state.rb
@@ -1,2 +1,32 @@
 class State < ActiveRecord::Base
+  require 'csv'
+
+  attr_reader :name_line
+
+    def open_file(filename)
+      CSV.open(filename, :headers => [], :header_converters => :symbol)
+    end
+
+    def parse_file(filename)
+      data = open_file(filename)
+      @name_line = data.map { |line| build_name_line(line)  }
+    end
+
+    def build_name_line(line)
+      SSABabyNameExporter::NameLine.new(:id => line[:id],
+                                         :state => line[:state],
+                                         :sex => line[:sex],
+                                         :year => line[:year],
+                                         :name => line[:name],
+                                         :count => line[:count])
+    end
+
+  # def self.import(file)
+  #   CSV.foreach(file.path, :headers => [], :header_converters => :symbol) do |row|
+  #     state_hash = row.to_hash
+  #     state = State.where(:id => product_hash[:id])
+  #     state.create!(state_hash)
+  #   end
+  # end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  resources :states
+
   namespace :api, :defaults => {:format => :json} do
     namespace :v1 do
       resources :states
