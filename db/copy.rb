d8f65644af7347b7dae8c5695094dbee200ce581
edatrix <lespath@gmail.com>
2015-03-21 23:26:10 -0600

    failed attempt at using COPY

---
+++ db/copy.rb
@@ -0,0 +1,29 @@
+conn = ActiveRecord::Base.connection
+rc = connection.raw_connection
+
+columns = [:state_name,:sex, :year, :name, :count]
+
+start_time = Time.now
+
+rc.transaction do
+  rc.exec("COPY namelines (#{columns.map(&:to_s).join(', ')}) FROM STDIN")
+
+  Dir.glob("./db/seeds/source/*").each do |state|
+    file = File.open('state', 'r')
+    index = 0
+  end
+
+  while !file.eof?
+    rc.put_copy_data(file.readline)
+  end
+
+  rc.put_copy_end
+
+  while res = rc.get_result
+    print e_message if e_message = res.error_message
+  end
+end
+
+finish_time = Time.now
+
+puts "time to import: #{finish_time-start_time}"
